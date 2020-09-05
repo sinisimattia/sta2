c3bd25a27976abac16d8cca3bae1acc1945033ed
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-09-06 00:12:38 +0200

    Correctly configured unit tests

--- jest.config.js
+++ jest.config.js
@@ -4,15 +4,12 @@ module.exports = {
     '^~/(.*)$': '<rootDir>/$1',
     '^vue$': 'vue/dist/vue.common.js'
   },
-  moduleFileExtensions: ['ts', 'js', 'vue', 'json'],
+  moduleFileExtensions: ['js', 'json'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
     '^.+\\.js$': 'babel-jest',
-    '.*\\.(vue)$': 'vue-jest'
   },
   collectCoverage: true,
   collectCoverageFrom: [
-    '<rootDir>/components/**/*.vue',
-    '<rootDir>/pages/**/*.vue'
+    '<rootDir>/classes/**/*.js'
   ]
 }
