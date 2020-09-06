e0cecd6b63c1f3d8564e9b257542ecb81760525d
Mattia Sinisi <mattia.sinisi@virgilio.it>
2020-09-06 15:02:25 +0200

    Separated sections into a separate component and added `id` prop

--- pages/index.vue
+++ pages/index.vue
@@ -20,40 +20,7 @@
         </div>
       </div>
 
-      <div
-        class="hero"
-        v-for="(section, index) in sections"
-        :key="index"
-        :class="section.type ? `is-${section.type}` : null"
-      >
-        <div class="hero-body">
-          <div class="container">
-            <div class="section">
-              <div class="columns is-vcentered">
-                <div class="column">
-                  <img :src="require(`~/assets/img/${section.image}`)" />
-                </div>
-                <div class="column">
-                  <p class="title is-1" v-if="section.title">
-                    {{ section.title }}
-                  </p>
-                  <p class="subtitle is-4" v-if="section.snippet">
-                    {{ section.snippet }}
-                  </p>
-                  <div class="is-content" v-if="section.content">
-                    <span
-                      v-for="(paragraph, p) in section.content.paragraphs"
-                      :key="p"
-                    >
-                      <p v-if="paragraph">{{ paragraph }}</p>
-                    </span>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <Sections :sections="sections" />
     </div>
   </div>
 </template>
@@ -62,8 +29,12 @@
 import { identity } from '~/snisni.config.json'
 import illustration from '~/assets/img/illustrations/work_together.svg'
 import { sections } from '~/config/home.json'
+import Sections from '@/components/organisms/Sections'
 
 export default {
+  components: {
+    Sections
+  },
   data() {
     return {
       title: identity.name,
